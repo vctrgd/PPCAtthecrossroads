2c007f4b11ff93bd24874f8048d7d0db08e12dd9
vctrgd <rigaudvictor13@gmail.com>
2025-02-04 17:46:30 +0100

    bonne coordoné des points

--- crossRoad.py
+++ crossRoad.py
@@ -2,15 +2,15 @@ roads = ["North", "South", "West", "East"]
 
 
 ENTRY_POINTS = {
-    "North": (400, 0),
-    "South": (400, 600),
+    "North": (400, 600),
+    "South": (400, 0),
     "East": (800, 300),
     "West": (0, 300)
 }
 
 DESTINATION_POINTS = {
-    "North": (400, 0),
-    "South": (400, 600),
+    "North": (400, 600),
+    "South": (400, 0),
     "East": (800, 300),
     "West": (0, 300)
 }
