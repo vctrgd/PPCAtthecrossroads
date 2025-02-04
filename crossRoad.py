03b6f73a895b3a82f70d1726898a0d90a83bc255
vctrgd <rigaudvictor13@gmail.com>
2025-02-04 17:25:05 +0100

    correction des positions

--- crossRoad.py
+++ crossRoad.py
@@ -2,15 +2,15 @@ roads = ["North", "South", "West", "East"]
 
 
 ENTRY_POINTS = {
-    "North": (400, 600),  # Centre haut (800x600)
-    "South": (400, 0),    # Centre bas
-    "East": (800, 300),   # Centre droit
-    "West": (0, 300)      # Centre gauche
+    "North": (400, 0),
+    "South": (400, 600),
+    "East": (800, 300),
+    "West": (0, 300)
 }
 
 DESTINATION_POINTS = {
     "North": (400, 0),
     "South": (400, 600),
-    "East": (0, 300),
-    "West": (800, 300)
+    "East": (800, 300),
+    "West": (0, 300)
 }
